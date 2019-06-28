a66a9e529f0995b95425909bc2c6e2245e9a43f8
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-06-28 15:59:13 +0200

    logging and checking further pod messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, os
+import time, os, re, sys, datetime
 
 import pykube
 from urllib3.exceptions import ProtocolError
@@ -8,32 +8,48 @@ NODENAME = os.environ.get("NODENAME")
 KUBECONFIG = os.environ.get("KUBECONFIG", "~/.kube/config")
 POLL_INTERVAL = int(os.environ.get("POLL_INTERVAL", "60"))
 
+MATCH_EVENTS = [
+  "rpc error",
+  "failed create pod sandbox",
+  "context deadline exceeded"
+]
+
+def log(msg):
+  print("\n[%s] %s" %(datetime.datetime.now(), msg))
+
 def main():
   if NODENAME is None:
     raise Exception("NODENAME cannot be None")
 
   api = pykube.HTTPClient(pykube.KubeConfig.from_file(KUBECONFIG))
 
-  print("Der node doktor ist in für %s, checkup every %s seconds..." % (NODENAME, POLL_INTERVAL))
+  sys.stdout.write("Der node doktor ist in für %s, checkup every %s seconds..." % (NODENAME, POLL_INTERVAL))
 
   while True:
     try:
-      found_bad_pods = False
+      bad_pods = []
 
-      for pod in pykube.Pod.objects(api).filter(field_selector={"spec.nodeName": NODENAME}):
+      for pod in pykube.Pod.objects(api).filter(field_selector={"spec.nodeName": NODENAME, "status.phase": "ContainerCreating"}):
         for pod_event in pykube.Event.objects(api).filter(field_selector={"involvedObject.name": pod.obj['metadata']['name']}):
-          if 'rpc error' in pod_event.obj['message']:
-            pod.delete()
-            found_bad_pods = True
-            break
+          for event in MATCH_EVENTS:
+            if re.search(event, pod_event.obj['message'], re.IGNORECASE):
+              log("Found event '%s' for pod %s" % (pod_event.obj['message'], pod.obj['metadata']['name']))
+              bad_pods.append(pod)
+              break
 
-      if found_bad_pods:
-        print("Found bad pods.  Cordoning node...")
+      if bad_pods:
+        log("Cordoning node and deleting pods...")
         pykube.Node.objects(api).get(name=NODENAME).cordon()
+        for pod in bad_pods:
+          pod.delete()
 
     except (ProtocolError, ConnectionResetError, ConnectionError, ReqConnectionError):
-      print("Connection reset...")
+      log("Connection reset...")
+      api = pykube.HTTPClient(pykube.KubeConfig.from_file(KUBECONFIG))
+      continue
 
+    sys.stdout.write('.')
+    sys.stdout.flush()
     time.sleep(POLL_INTERVAL)
 
 if __name__ == "__main__":
